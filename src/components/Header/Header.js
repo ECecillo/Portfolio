c19906df25862ab80a35bef681c551b3b9452d56
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-01-13 19:04:42 +0100

    Container : Composant dans lequel on va envelopper les éléments de la navbar.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,14 @@ import react from "react";
 // module de nextjs permettant de faire les liens entre les pages plus facilement avec le composant <Link>
 import Link from "next/link"; 
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { Container } from "../../layout/LayoutStyles";
 
 
 
 const Header = () => (
-
+    <Container>
+        
+    </Container>
 );
 
 export default Header;
