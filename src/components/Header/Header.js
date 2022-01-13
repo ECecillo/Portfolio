3bf5f0e4158814f92f46f9ac9b52d008d3b700cc
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-01-13 18:40:26 +0100

    ajout de react-icons au projet pour inclure plus facilement les icons du type fontawesome material .... On créer également un fichier HeaderStyles qui s'occupera du style des composants de Header.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import react from "react";
+// module de nextjs permettant de faire les liens entre les pages plus facilement avec le composant <Link>
+import Link from "next/link"; 
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+
+
+const Header = () => (
+
+);
+
+export default Header;
