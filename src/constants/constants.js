cb0a412619c0b4843c059bd426c9ad52e5fa17ad
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-02-23 22:36:10 +0100

    Ajouts des traductions pour le titre et la description dans constant.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,23 +1,41 @@
 export const projects = [
   {
-    title: 'Gestionnaire de QCM API',
-    description: "Gestion des réponses/requêtes vers une API avec JS, affichages, créations, modifications des données à partir de la page.",
+    fr : {
+      title: 'Gestionnaire de QCM API',
+      description: "Gestion des requêtes/réponses vers une API avec JS, affichages, créations, modifications des données à partir de la page.",
+    },
+    en : {
+      title: "Questionnary API",
+      description: "Fetch Data from an API and give you the oportunity to update, delete, and create new questions."
+    },
     image: '/images/1.png',
     tags: ['JS', 'HTML', 'CSS'],
     source: 'https://github.com/ECecillo/Questionnary_API',
     id: 0,
   },
   {
-    title: 'Portfolio',
-    description: "En partant d'un design fait sur Figma j'ai du apprendre les frameworks React, NextJS et Styled Component pour réaliser ce projet..",
+    fr : {      
+      title: 'Portfolio',
+      description: "En partant d'un design fait sur Figma j'ai du apprendre les frameworks React, NextJS et Styled Component pour réaliser ce projet..",
+    },
+    en : {
+      title:"Portfolio",
+      description:"Using Figma for the Design model, I learned how to use React, NextJS and Styled Component to build this site.",
+    },
     image: '/images/2.png',
     tags: ['React', 'NextJS', 'Styled Component', 'JS', 'CSS', 'Figma'],
     source: 'https://github.com/ECecillo/Portfolio',
     id: 1,
   },
   {
-    title: 'Star wars Scene',
-    description: "Création d'une scène avec des objets 3D en utilisant la librairie GKIT.",
+    fr : {
+      title: 'Star wars Scene',
+      description: "Création d'une scène avec des objets 3D, une cubemap, un Billboard, des images animés en utilisant la librairie GKIT.",
+    },
+    en : {
+      title: "Star wars Scene",
+      description: "Render a Static Scene with some 3D imported objets, a Cubemap, a Billboard and some images using GKIT."
+    },
     image: '/images/3.png',
     tags: ['C++'],
     source: 'https://github.com/ECecillo/Star_Wars_Scene_GKIT',
@@ -25,8 +43,14 @@ export const projects = [
     id: 2,
   }, 
   {
-    title: 'PinterestLike',
-    description: "Reprend le principe de Pinterest avec la visualisation d'images stockés sur une BDD. On gère également l'inscription, la connexion d'utilisateurs.",
+    fr : {
+      title: 'PinterestLike',
+      description: "Reprend le principe de Pinterest avec la visualisation d'images stockés sur une Base de Données MySQL. On gère également l'inscription, la connexion d'utilisateurs en PHP.",
+    },
+    en : {
+      title: "PinterestLike",
+      description: "As in the famous website Pinterest, we show the pictures uploaded by some users stored in a database using PHP/MySQL."
+    },
     image: '/images/4.png',
     tags: ['SQL', 'PHP', 'CSS'],
     source: 'https://github.com/ECecillo/PinterestLike',
@@ -34,8 +58,14 @@ export const projects = [
     id: 3,
   },
   {
-    title: 'Uno',
-    description: "Reproduction du jeux de cartes du Uno avec ces variantes et la possibilité de jouer avec une IA. Il y a 2 versions, une version texte et une version Graphique.",
+    fr : {
+      title: 'Uno',
+      description: "Reproduction du jeux de cartes du Uno avec ces variantes et la possibilité de jouer avec une IA. Il y a 2 versions, une version texte et une version Graphique.",
+    },
+    en : {
+      title: "Uno",
+      description: "Trying to reproduce the famous game of Uno with its variant and the possibility to play with multiple AI. There are two versions, one Graphical and one in the CLI."
+    },
     image: '/images/5.png',
     tags: ['C++', 'SDL'],
     source: 'https://github.com/ECecillo/Uno',
@@ -43,8 +73,14 @@ export const projects = [
     id: 4,
   },
   {
-    title: 'A* Pathfinding',
-    description: "Application de l'algorithme A* avec une visualisation du résultat finale dans le Terminale.",
+    fr : {
+      title: 'A* Pathfinding',
+      description: "Application de l'algorithme A* avec une visualisation du résultat finale dans le Terminale.",
+    },
+    en : {
+      title: "A* Pathfinding",
+      description: "Applying the principle of the Astar algorithm and visualizing the result in the CLI."
+    },
     image: '/images/6.png',
     tags: ['C++'],
     source: 'https://github.com/ECecillo/Astar_Pathfinding',
@@ -52,8 +88,14 @@ export const projects = [
     id: 5,
   },
   {
-    title: 'Memory',
-    description: "Site qui regroupe des jeux de mémoires, réalisés avec HTML/CSS et JS Vanilla.",
+    fr : {
+      title: 'Memory',
+      description: "Site qui regroupe des jeux de mémoires, réalisés avec HTML/CSS et JS Vanilla.",
+    },
+    en : {
+      title: "Memory",
+      description: "A website that groups some Memory games, realised in HTML/CSS and Vanilla JS."
+    },
     image: '/images/7.png',
     tags: ['HTML', 'CSS', 'JS'],
     source: 'https://github.com/ECecillo/MemoryWeb',
@@ -61,8 +103,14 @@ export const projects = [
     id: 6,
   },
   {
-    title: 'Arbre Rouge et Noir',
-    description: "Visualisation et mesure des performances entre un Arbre Rouge et Noir et un Arbre Binaire de Recherche.",
+    fr: {
+      title: 'Arbre Rouge et Noir',
+      description: "Visualisation et mesure des performances entre un Arbre Rouge et Noir et un Arbre Binaire de Recherche.",
+    },
+    en : {
+      title: "Red Black Tree",
+      description: "Visualizing and measuring the performances between a Red-Black Tree and a BST."
+    },
     image: '/images/8.png',
     tags: ['C++'],
     source: 'https://github.com/ECecillo/Red_Black_Tree',
@@ -70,12 +118,18 @@ export const projects = [
     id: 7,
   },
   {
-    title: 'Blog Prisma',
-    description: "Blog basique fait avec NextJS et Prisma en ORM.",
+    fr : {
+      title: 'Blog Prisma',
+      description: "Blog basique fait avec NextJS et Prisma en ORM.",
+    },
+    en : {
+      title: "Prisma Blog Poster",
+      description: "Basic Blog using NextJS and Prisma."
+    },
     image: '/images/9.png',
-    tags: ['JS', 'NEXT', 'CSS'],
+    tags: ['JS', 'NEXT', 'CSS', 'Prisma'],
     source: 'https://github.com/ECecillo/ececillo-blogr-nextjs-prisma',
-    visit: '',
+    visit: 'https://ececillo-blogr-nextjs-prisma.vercel.app/',
     id: 0,
   }
 ];
