1d458a9562a2e96582c56ee82b444da691109860
ECecillo <47942402+ECecillo@users.noreply.github.com>
2022-06-17 12:34:14 +0200

    feat: modification du message de recherche de stage

--- src/components/SpecialText/SpecialText.js
+++ src/components/SpecialText/SpecialText.js
@@ -5,7 +5,7 @@ import { Container, Head1, HireButton } from './SpecialTextStyles';
 export default function SpecialText(props) {
     const text = "#OPENTOWORK";
     console.log(props.language);
-    const Hire_Text = props.language === "fr" ? "Je cherche un Stage !" : "I am looking for an Internship !";
+    const Hire_Text = props.language === "fr" ? "Je cherche une Alternance !" : "I am looking for a study training program !";
     const Linkedin_Link = "https://www.linkedin.com/in/ececillo/";
     const Linkedin_Page = props.language !== "fr" ? Linkedin_Link + "?locale=en_US" : Linkedin_Link;
     return (
