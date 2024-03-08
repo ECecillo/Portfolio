83f7b3bffed95e33b6f4bc57e2ba3acf169f5894
ECecillo <enzocecillon@laptop-8fk0oir7.univ-lyon1.fr>
2024-03-08 17:49:09 +0100

    chore: move app to components folder to respect project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App.tsx';
+import App from '@/ui/components/app/App';
 import '@/ui/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
