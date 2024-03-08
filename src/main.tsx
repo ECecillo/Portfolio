5792b325c5d9d8e6e72029ce5a1f1dd889fd06ce
ECecillo <enzocecillon@laptop-8fk0oir7.univ-lyon1.fr>
2024-03-08 18:08:18 +0100

    chore: add tailwind as css framework to build ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/ui/components/app/App';
-import '@/ui/styles/index.css';
+import '@/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/ui/components/app/App.tsx
+++ src/ui/components/app/App.tsx
@@ -1,11 +1,3 @@
-import '@/ui/styles/App.css';
-
-function App() {
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
